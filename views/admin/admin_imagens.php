9a919c50a88bd2fe62258d8d2931ded2c2c1be0c
lippekstro <fellipecastro13@hotmail.com>
2023-02-06 13:45:41 -0300

    arrumando um erro

--- views/admin/admin_imagens.php
+++ views/admin/admin_imagens.php
@@ -10,37 +10,31 @@ try {
 
 ?>
 
-<?php if (count($lista) > 0) : ?>
-    <section class="container-table">
-        <table>
-            <caption>Administração de Imagens da Página Inicio</caption>
+<section class="container-table">
+    <table>
+        <caption>Administração de Imagens da Página Inicio</caption>
+        <tr>
+            <th>imagem</th>
+            <th>data de upload</th>
+            <th>
+                <a href="/agitos_slz/views/admin/adicionar_imagem_inicio.php">
+                    <span class="material-symbols-outlined">add</span>
+                </a>
+            </th>
+        </tr>
+        <?php foreach ($lista as $img) : ?>
             <tr>
-                <th>imagem</th>
-                <th>data de upload</th>
-                <th>
-                    <a href="/agitos_slz/views/admin/adicionar_imagem_inicio.php">
-                        <span class="material-symbols-outlined">add</span>
+                <td><img src="data:image/jpg;charset=utf8;base64,<?= base64_encode($img['imagem_inicio']); ?>" alt=""></td>
+                <td><?= $date->format(new DateTime($img['data_upload'])) ?></td>
+                <td>
+                    <a href="/agitos_slz/controllers/deletar_imagem_controller.php?id_imagem=<?= $img['id_imagem'] ?>">
+                        <span class="material-symbols-outlined">delete</span>
                     </a>
-                </th>
+                </td>
             </tr>
-            <?php foreach ($lista as $img) : ?>
-                <tr>
-                    <td><img src="data:image/jpg;charset=utf8;base64,<?= base64_encode($img['imagem_inicio']); ?>" alt=""></td>
-                    <td><?= $date->format(new DateTime($img['data_upload'])) ?></td>
-                    <td>
-                        <a href="/agitos_slz/controllers/deletar_imagem_controller.php?id_imagem=<?= $img['id_imagem'] ?>">
-                            <span class="material-symbols-outlined">delete</span>
-                        </a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </section>
-<?php else : ?>
-    <section>
-        Ainda não há imagens cadastradas
-    </section>
-<?php endif; ?>
+        <?php endforeach; ?>
+    </table>
+</section>
 
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . '/agitos_slz/templates/rodape.php';
--- views/admin/admin_postagens_blog.php
+++ views/admin/admin_postagens_blog.php
@@ -10,44 +10,38 @@ try {
 
 ?>
 
-<?php if (count($lista) > 0) : ?>
-    <section class="container-table">
-        <table>
-            <caption>Administração do Blog</caption>
+<section class="container-table">
+    <table>
+        <caption>Administração do Blog</caption>
+        <tr>
+            <th>Título</th>
+            <th>Data de Publicação</th>
+            <th>Autor</th>
+            <th colspan="2">
+                <a href="/agitos_slz/views/admin/adicionar_postagem_blog.php">
+                    <span class="material-symbols-outlined">add</span>
+                </a>
+            </th>
+        </tr>
+        <?php foreach ($lista as $post) : ?>
             <tr>
-                <th>Título</th>
-                <th>Data de Publicação</th>
-                <th>Autor</th>
-                <th colspan="2">
-                    <a href="/agitos_slz/views/admin/adicionar_postagem_blog.php">
-                        <span class="material-symbols-outlined">add</span>
+                <td><?= $post['titulo'] ?></td>
+                <td><?= $datahora->format(new DateTime($post['data_publicacao'])) ?></td>
+                <td><?= $post['nome_autor'] ?></td>
+                <td>
+                    <a href="/agitos_slz/views/admin/editar_post.php?id_post=<?= $post['id_post'] ?>">
+                        <span class="material-symbols-outlined">edit</span>
                     </a>
-                </th>
+                </td>
+                <td>
+                    <a href="/agitos_slz/controllers/deleta_post_controller.php?id_post=<?= $post['id_post'] ?>">
+                        <span class="material-symbols-outlined">delete</span>
+                    </a>
+                </td>
             </tr>
-            <?php foreach ($lista as $post) : ?>
-                <tr>
-                    <td><?= $post['titulo'] ?></td>
-                    <td><?= $datahora->format(new DateTime($post['data_publicacao'])) ?></td>
-                    <td><?= $post['nome_autor'] ?></td>
-                    <td>
-                        <a href="/agitos_slz/views/admin/editar_post.php?id_post=<?= $post['id_post'] ?>">
-                            <span class="material-symbols-outlined">edit</span>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="/agitos_slz/controllers/deleta_post_controller.php?id_post=<?= $post['id_post'] ?>">
-                            <span class="material-symbols-outlined">delete</span>
-                        </a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </section>
-<?php else : ?>
-    <section>
-        Ainda não há postagens cadastradas
-    </section>
-<?php endif; ?>
+        <?php endforeach; ?>
+    </table>
+</section>
 
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . '/agitos_slz/templates/rodape.php';
