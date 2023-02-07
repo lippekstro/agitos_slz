4eea081991d586a8469375c5246b7c3e0c708050
lippekstro <fellipecastro13@hotmail.com>
2023-02-07 10:15:56 -0300

    mais bugs

--- views/eventos.php
+++ views/eventos.php
@@ -10,43 +10,13 @@ try {
 ?>
 
 <?php if (count($lista) > 0) : ?>
-<section>
-    <div class="slideshow-container">
-        <?php for ($i = 0; $i < 4; $i++) : ?>
-            <div class="mySlides efeito">
-                <img src="data:image/jpg;charset=utf8;base64,<?= base64_encode($lista[$i]['imagem_evento']); ?>" width="100%">
-            </div>
-        <?php endfor; ?>
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <br>
-
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-        <span class="dot" onclick="currentSlide(4)"></span>
-    </div>
-</section>
-
-<section class="corpo">
-    <div class="especialidades">
-        <div id="titulo-evento">
-            <h5> SE LIGA NA AGENDA CULTURAL DA ILHA DO AMOR</h5>
-        </div>
-
-        <div class="conteinerespecialidades">
-            <?php foreach ($lista as $evento) : ?>
-                <div class="esp">
-                    <a href="/agitos_slz/views/evento_exibe.php?id_evento=<?= $evento['id_evento'] ?>">
-                        <img src="data:image/jpg;charset=utf8;base64,<?= base64_encode($evento['imagem_evento']); ?>" alt="">
-                        <p>
-                            <?= $evento['nome_evento'] ?> / <?= $date->format(new DateTime($evento['data_evento'])) ?>
-                        </p>
-                    </a>
+    <section>
+        <div class="slideshow-container">
+            <?php for ($i = 0; $i < 4; $i++) : ?>
+                <div class="mySlides efeito">
+                    <img src="data:image/jpg;charset=utf8;base64,<?= base64_encode($lista[$i]['imagem_evento']); ?>" width="100%">
                 </div>
-            <?php endforeach; ?>
+            <?php endfor; ?>
             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
             <a class="next" onclick="plusSlides(1)">&#10095;</a>
         </div>
@@ -62,7 +32,7 @@ try {
 
     <section class="corpo">
         <div class="especialidades">
-            <div id="titulo">
+            <div id="titulo-evento">
                 <h5> SE LIGA NA AGENDA CULTURAL DA ILHA DO AMOR</h5>
             </div>
 
@@ -78,8 +48,8 @@ try {
                     </div>
                 <?php endforeach; ?>
             </div>
-        </div>
     </section>
+
 <?php else : ?>
     <section>
         Ainda não há eventos cadastrados
